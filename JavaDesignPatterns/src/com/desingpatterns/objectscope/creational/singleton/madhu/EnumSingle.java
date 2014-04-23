f2c5f452b6e48b4da67c7d3a0c6ff88f7a86bd8e
gitmadhu <madhub2v@gmail.com>
2014-04-23 21:24:22 +0530

    enum singleton

---
+++ JavaDesignPatterns/src/com/desingpatterns/objectscope/creational/singleton/madhu/EnumSingle.java
@@ -0,0 +1,10 @@
+package com.desingpatterns.objectscope.creational.singleton.madhu;
+
+public enum EnumSingle {
+	INSTANCE;
+	
+	public void doSomeWork(){
+		System.out.println("this adds some behaviour to the object");
+	}
+
+}
---
+++ JavaDesignPatterns/src/com/desingpatterns/objectscope/creational/singleton/madhu/EnumSingleTest.java
@@ -0,0 +1,21 @@
+package com.desingpatterns.objectscope.creational.singleton.madhu;
+
+public class EnumSingleTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EnumSingle singletonObj = EnumSingle.INSTANCE;
+		singletonObj.doSomeWork();
+		
+		EnumSingle singletonObj1 = EnumSingle.INSTANCE;
+		EnumSingle singletonObj2 = EnumSingle.INSTANCE;
+		EnumSingle singletonObj3 = EnumSingle.INSTANCE;
+		
+		System.out.println(singletonObj1);
+		System.out.println(singletonObj2);
+		System.out.println(singletonObj3);
+	}
+
+}
