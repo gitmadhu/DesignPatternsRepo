656cae7ec73542119f2808c86cac377718c042d6
Gopakumar <gopakumar.kondayin@gmail.com>
2014-04-30 12:14:57 +0530

    List to Map Updated

--- JavaDesignPatterns/src/com/designpatterns/objectstructural/adaptor/mani/ListToMap.java
+++ JavaDesignPatterns/src/com/designpatterns/objectstructural/adaptor/mani/ListToMap.java
@@ -3,18 +3,19 @@ package com.designpatterns.objectstructural.adaptor.mani;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-public class Adaptors {
+import java.util.Map;
+public class ListToMap {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<String> list = new ArrayList<String>();
+		List<String> list = new ArrayList<>();
 		list.add("First");
 		list.add("Second");
 		list.add("Third");
 		System.out.println("This is Hash Map after Conversion From List..."+adaptor(list));
 	}
-	private static HashMap<Integer, String> adaptor(List<String> list) {
+	private static Map<Integer, String> adaptor(List<String> list) {
 		// TODO Auto-generated method stub
-		HashMap<Integer, String> hashmap = new HashMap<Integer, String>();
+		Map<Integer, String> hashmap = new HashMap<>();
 		for (String string : list) {
 			hashmap.put(list.indexOf(string), string);
 		}
