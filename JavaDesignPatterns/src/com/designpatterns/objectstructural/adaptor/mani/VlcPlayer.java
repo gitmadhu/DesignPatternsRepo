c17c5bdc211f7b9efe720dc03dc014d566c87e53
manikanta <pavana@neevtech.com>
2014-05-02 11:15:10 +0530

    Updated

--- JavaDesignPatterns/src/com/designpatterns/objectstructural/adaptor/mani/VlcPlayer.java
+++ JavaDesignPatterns/src/com/designpatterns/objectstructural/adaptor/mani/VlcPlayer.java
@@ -5,7 +5,7 @@ public class VlcPlayer implements AdvancedMediaPlayer{
 	@Override
 	public void playVlc(String fileName) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Playing vlc file. Name: "+ fileName);	
 	}
 
 	@Override
